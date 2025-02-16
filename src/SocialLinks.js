57b7a14694f2479e35513e31ea72548b956e44e8
Thota Bharath Kumar <bharatht336@gmail.com>
2025-02-16 17:12:45 +0530

    added new resume

--- src/SocialLinks.js
+++ src/SocialLinks.js
@@ -9,7 +9,7 @@ const SocialLinks = () => {
       <a href="https://www.linkedin.com/in/pallavi-yellisetty-080451223?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" rel="noopener noreferrer">
         <img src="/icons/linkedin-icon.svg" alt="LinkedIn" />
       </a>
-      <a href="/Resume/MyResume.pdf" target="_blank" rel="noopener noreferrer">
+      <a href="/Resume/YellisettyPallaviResume.pdf" target="_blank" rel="noopener noreferrer">
         <img src="/icons/resume-icon.svg" alt="Resume" />
       </a>
     </div>
